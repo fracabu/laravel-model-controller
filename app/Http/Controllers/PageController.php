ae5da98223242027b1d4a543c6bed6b1b4eafff3
fracabu <100199965+fracabu@users.noreply.github.com>
2022-07-28 19:23:25 +0200

    mod

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Page;
 class PageController extends Controller
 {
+
     function index () {
+
+        $data = Page::all();
+        dump($data);
         return view('index');
     }
 }
