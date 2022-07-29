a5c1d7050a7dc0409d0dbe11dcb865dc510cf993
fracabu <100199965+fracabu@users.noreply.github.com>
2022-07-29 17:36:43 +0200

    mod

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Page;
+use App\Movie;
 class PageController extends Controller
 {
 
     function index () {
 
-        $data = Page::all();
+        $data = Movie::all();
         dump($data);
         return view('index');
     }
